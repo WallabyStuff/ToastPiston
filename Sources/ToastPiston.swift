07a8c55fb044fe146f496c10fcda274dbfb74203
Wallaby <avocado34.131@gmail.com>
2023-08-05 16:03:41 +0900

    [Rename] Integrate spring animation method

--- Sources/ToastPiston.swift
+++ Sources/ToastPiston.swift
@@ -186,3 +186,21 @@ extension UIViewController {
     return false
   }
 }
+
+
+// MARK: - Spring animation
+
+extension UIView {
+  static func springAnimate(withDuration: CGFloat,
+                            animations: @escaping () -> Void,
+                            completion: (() -> Void)? = nil) {
+    animate(withDuration: withDuration,
+            delay: 0,
+            usingSpringWithDamping: 0.9,
+            initialSpringVelocity: 0.9) {
+      animations()
+    } completion: { _ in
+      completion?()
+    }
+  }
+}
